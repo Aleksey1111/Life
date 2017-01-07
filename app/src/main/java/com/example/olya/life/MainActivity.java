199fb6ab0ba28e65c1b7eb914e2708db670bd360
Aleksey1111 <sla1411@ukr.net>
2017-01-07 23:01:31 +0200

    added toolbar

--- app/src/main/java/com/example/olya/life/MainActivity.java
+++ app/src/main/java/com/example/olya/life/MainActivity.java
@@ -2,12 +2,28 @@ package com.example.olya.life;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
-
+private Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+                initToolbar();
+    }
+
+    private void initToolbar() {
+    toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.setTitle);
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                return false;
+            }
+        });
+        toolbar.inflateMenu(R.menu.menu);
+
     }
 }
