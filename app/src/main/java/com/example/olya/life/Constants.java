b7a252c8aeb76fe5c3dcff6f3fb1c84fec043bf4
Aleksey1111 <sla1411@ukr.net>
2017-01-10 19:09:45 +0200

    imolemended navigation view and menu nav

---
+++ app/src/main/java/com/example/olya/life/Constants.java
@@ -0,0 +1,11 @@
+package com.example.olya.life;
+
+/**
+ * Created by Olya on 10.01.2017.
+ */
+
+public class Constants {
+    public static  final  int TAB1 = 0;
+    public static  final  int TAB2 = 1;
+    public static  final  int TAB3 = 2;
+}
--- app/src/main/java/com/example/olya/life/MainActivity.java
+++ app/src/main/java/com/example/olya/life/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.olya.life;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -49,7 +52,28 @@ public class MainActivity extends AppCompatActivity {
 
     private void initNavigationView() {
         drawerLayout = (DrawerLayout) findViewById(R.id.drawer);
+        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar, R.string.open,R.string.close);
+        drawerLayout.setDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.navigation);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                drawerLayout.closeDrawers();
+                switch (item.getItemId()){
+                    case R.id.not:
+                        ShowNotification();
+
+                }
+                return true;
+            }
+        });
     }
 
 
+
+private void ShowNotification(){
+    viewPager.setCurrentItem(Constants.TAB2);
+}
 }
